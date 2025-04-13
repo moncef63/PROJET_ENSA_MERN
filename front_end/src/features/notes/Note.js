161035f7cceb71ade64311f4dee5ac3cc2b790e6
Moncif ayouch <ayouch.moncif@gmail.com>
2025-04-13 13:46:08 +0100

    fix: update date formatting to use French locale in Note component

--- front_end/src/features/notes/Note.js
+++ front_end/src/features/notes/Note.js
@@ -12,9 +12,9 @@ const Note = ({ noteId }) => {
     const navigate = useNavigate()
 
     if (note) {
-        const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
+        const created = new Date(note.createdAt).toLocaleString('fr-MA', { day: 'numeric', month: 'long' })
 
-        const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
+        const updated = new Date(note.updatedAt).toLocaleString('fr-MA', { day: 'numeric', month: 'long' })
 
         const handleEdit = () => navigate(`/dash/notes/${noteId}`)
 
